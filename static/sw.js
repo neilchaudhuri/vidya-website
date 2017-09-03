861ac28e4cd8b41d90a244b048a9286d94910abe
Neil Chaudhuri <neil@vidyasource.com>
2017-09-02 20:54:40 -0400

    Use all min

--- static/sw.js
+++ static/sw.js
@@ -14,7 +14,7 @@ const CACHE_FILES = [
   "//cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0/jquery.counterup.min.js",
   "//cdnjs.cloudflare.com/ajax/libs/jquery-parallax/1.1.3/jquery-parallax.js",
   "//cdn.jsdelivr.net/npm/lazyload@2.0.0-beta.1/lazyload.js",
-  '/js/all.js',
+  '/js/all.min.js',
   '/manifest.json',
   '/404.html',
   '/?utm_source=homescreen',
